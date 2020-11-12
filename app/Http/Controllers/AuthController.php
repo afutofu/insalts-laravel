b561b0e38b082d3ce36fdfcfbabac654c879d834
Afuza <theafuza@gmail.com>
2020-11-12 11:45:00 +0700

    used validator class for server side validation

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,29 +4,32 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 use App\Models\User;
 
 class AuthController extends Controller
 {
     public function login (Request $request){
+
+        $validator = Validator::make($request->all(), 
+                    [ 
+                    'email' => 'required|email|exists:users',
+                    'password' => 'required', 
+                    ]);  
+
+         if ($validator->fails()) {  
+            return response()->json(['error'=>$validator->errors()], 400); 
+        }  
+
         // Retreive request data
         $email = $request->input('email');
         $password = $request->input('password');
+
     
-        // Validation
-        // Check if fields are empty
-        if (!$email || !$password){
-            return response()->json(["msg"=>"Please enter all fields"], 400);
-        }
-    
-        // Check if user exists
+        // Retreive user
         $foundUser = User::where('email', $email)->first();
-        if (!$foundUser){
-            return response()->json(["msg"=>"User does not exist"], 400);
-        }
-    
-        // If user exists
+
         // Verify password
         $hashedPassword = $foundUser->password;
         if (Hash::check("$password", $hashedPassword)) {
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 use App\Models\User;
 
@@ -11,37 +12,23 @@ use App\Models\User;
 class UserController extends Controller
 {
     public function store(Request $request){
+
+        $validator = Validator::make($request->all(), 
+                    [ 
+                    'username'=> 'required|min:4|alpha_num',
+                    'email' => 'required|email|unique:users',
+                    'password' => 'required|min:8',
+                    'retyped_password' => 'required|same:password'
+                    ]);  
+
+         if ($validator->fails()) {  
+            return response()->json(['error'=>$validator->errors()], 400); 
+        }  
+
         // Retreive request data
         $username = $request->input('username');
         $email = $request->input('email');
         $password = $request->input('password');
-
-        // Server-Side Validation
-        // Check if fields are empty
-        if (!$username || !$email || !$password){
-            return response()->json(["msg"=>"Please enter all fields"], 400);
-        }
-
-        // Check username length
-        if (strlen($username) < 4){
-            return response()->json(["msg"=>"Username must be at least 4 characters long"], 400);
-        }
-
-        // Validate email
-        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            return response()->json(["msg"=>"Invalid email format"], 400);
-        }
-
-        // Check password length
-        if (strlen($username) < 8){
-            return response()->json(["msg"=>"Password must be at least 8 characters long"], 400);
-        }
-    
-        // Check if user exists
-        $foundUser = User::where('email', $email)->first();
-        if ($foundUser){
-            return response()->json(["msg"=>"Email already taken"], 400);
-        }
 
         // Create new user
         $user = new User();
